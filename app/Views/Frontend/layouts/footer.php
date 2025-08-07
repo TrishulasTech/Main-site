881e17fb6d08c1c1239c323c35fad4205915137e
Ratikant Patil <ratikantp71@gmail.com>
2025-08-07 14:49:37 +0530

    Initial commit

---
+++ app/Views/Frontend/layouts/footer.php
@@ -0,0 +1,91 @@
+
+    <footer class="footer-section">
+        <div class="container">
+            <div class="row footer-area">
+                <div class="col-lg-3 col-sm-6 sm-padding">
+                    <div class="footer-widget">
+                        <a class='footer-logo' href='index.html'><img src="<?= base_url('assets/img/logo-dark.png" alt="logo')?>"></a>
+                        <p>Our success in creating business solutions is due in part spacially to talented committed team.</p>
+                        <ul class="footer-social">
+                            <li><a href="#"><i class="lab la-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="lab la-twitter"></i></a></li>
+                            <li><a href="#"><i class="lab la-youtube"></i></a></li>
+                            <li><a href="#"><i class="lab la-behance"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 sm-padding">
+                    <div class="footer-widget">
+                        <h3>Company</h3>
+                        <ul class="footer-list">
+                            <li><a href='about.html'>About Us</a></li>
+                            <li><a href='contact.html'>Get in Touch</a></li>
+                            <li><a href='faqs.html'>Help &amp; Faqs</a></li>
+                            <li><a href='faqs.html'>Privacy Policy</a></li>
+                            <li><a href='contact.html'>Support</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 sm-padding">
+                    <div class="footer-widget">
+                        <h3>Information</h3>
+                        <ul class="footer-list">
+                            <li>
+                                <p>3770 Hidden Meadow Drive Venturia, ND 58489</p>
+                            </li>
+                            <li><a href="tel:7045550127">(704) 555-0127</a></li>
+                            <li><a href="#"><span class="__cf_email__" data-cfemail="edbe828b998a8883ad88958c809d8188c38e8280">[email&#160;protected]</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 sm-padding">
+                    <div class="footer-widget subscribe">
+                        <img class="subscribe-img" src="<?= base_url('assets/img/illustration06.png')?>" alt="illustration">
+                        <form action="https://te.dynamiclayers.net/softgen/php/mailchimp-subscribe.php" id="ajax_mc_form" class="subscribe-form">
+                            <div class="mc-fields">
+                                <input id="mc_email" class="form-control" type="email" name="mc_email" placeholder="Your Email" required>
+                                <button class="submit">Subscribe</button>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div id="mc-form-messages" class="alert" role="alert"></div>
+                        </form>
+                        <p>Subscribe us and get all the benifits from today.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright-text">
+                © <span id="currentYear"></span> Softgen, All Rights Reserved. Design By <a href="https://themeforest.net/user/themeeaster/portfolio" target="_blank">ThemeEaster</a>
+            </div>
+        </div>
+    </footer>
+    <!--/.footer-section-->
+
+    <div id="scrollup">
+        <button id="scroll-top" class="scroll-to-top"><i class="las la-arrow-up"></i></button>
+    </div>
+    <!--scrollup-->
+
+    <!--jQuery Lib-->
+    <script data-cfasync="false" src="<?= base_url('cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')?>"></script><script src="<?= base_url('assets/js/vendor/jquary-3.6.0.min.js')?>"></script>
+    <script src="<?= base_url('assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/atropos.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/bootstrap.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/popper.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/odometer.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/waypoints.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/venobox.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/swiper.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/smooth-scroll.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/wow.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/animated-text.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/gsap.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/scroll-trigger.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/vendor/parallax-scroll.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/mailchimp.js') ?>"></script>
+    <script src="<?= base_url('assets/js/main.js') ?>"></script>
+
+<script>(function(){function c(){var b=a.contentDocument||a.contentWindow.document;if(b){var d=b.createElement('script');d.innerHTML="window.__CF$cv$params={r:'96b549a53b7adb88',t:'MTc1NDU1NDEyMy4wMDAwMDA='};var a=document.createElement('script');a.nonce='';a.src='<?= base_url('cdn-cgi/challenge-platform/h/b/scripts/jsd/8359bcf47b68/maind41d.js')?>';document.getElementsByTagName('head')[0].appendChild(a);";b.getElementsByTagName('head')[0].appendChild(d)}}if(document.body){var a=document.createElement('iframe');a.height=1;a.width=1;a.style.position='absolute';a.style.top=0;a.style.left=0;a.style.border='none';a.style.visibility='hidden';document.body.appendChild(a);if('loading'!==document.readyState)c();else if(window.addEventListener)document.addEventListener('DOMContentLoaded',c);else{var e=document.onreadystatechange||function(){};document.onreadystatechange=function(b){e(b);'loading'!==document.readyState&&(document.onreadystatechange=e,c())}}}})();</script></body>
+
+
+<!-- Mirrored from te.dynamiclayers.net/softgen/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 07 Aug 2025 08:09:13 GMT -->
+</html>
---
+++ app/Views/Frontend/layouts/header.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Softgen Software and Technology HTML5 Template">
+    <meta name="author" content="ThemeEaster">
+
+    <title>Softgen | Software &amp; Technology HTML5 Template</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url('assets/img/favicon.png')?>">
+
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css')?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/animate.min.css')?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/keyframe-animation.css')?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/line-awesome.min.css')?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/odometer.min.css')?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/swiper.min.css')?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/venobox.min.css')?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/animated-text.min.css')?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/common-style.css')?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/main.css')?>">
+
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <div class="site-preloader">
+        <div class="spinner"></div>
+    </div>
+    <!-- Site Preloader -->
+
+    <header class="main-header transparent">
+        <div class="container">
+            <div class="navigation-wrapper">
+                <div class="navigation-inner">
+                    <div class="site-logo">
+                        <a href='index.html'><img src="<?= base_url('assets/img/logo-dark.png')?>" alt="Softgen"></a>
+                    </div>
+                    <nav class="navigation-menu">
+                        <ul class="main-menu">
+                            <li><a href='index.html'>Home</a>
+                                <ul>
+                                    <li><a href='index.html'>Marketing Startup</a></li>
+                                    <li><a href='index-2.html'>App Promotion</a></li>
+                                    <li><a href='index-3.html'>Software Saas</a></li>
+                                    <li><a href='index-4.html'>It Solutions</a></li>
+                                </ul>
+                            </li>
+                            <li><a href='about.html'>Company</a>
+                                <ul>
+                                    <li><a href='about.html'>About Us</a></li>
+                                    <li><a href='services.html'>Our Services</a></li>
+                                    <li><a href='service-details.html'>Services Details</a></li>
+                                    <li><a href='team.html'>Our Team</a></li>
+                                    <li><a href='team-details.html'>Team Details</a></li>
+                                    <li><a href='pricing.html'>Plans &amp; Pricing</a></li>
+                                    <li><a href='testimonial.html'>Testimonials</a></li>
+                                    <li><a href='faqs.html'>Help &amp; FAQ's</a></li>
+                                    <li><a href='404.html'>404 Error</a></li>
+                                </ul>
+                            </li>
+                            <li><a href='projects.html'>Projects</a>
+                                <ul>
+                                    <li><a href='projects.html'>Our Projects</a></li>
+                                    <li><a href='project-details.html'>Project Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href='blog-grid.html'>Blog</a>
+                                <ul>
+                                    <li><a href='blog-grid.html'>Blog Grid</a></li>
+                                    <li><a href='blog-classic.html'>Blog Classic</a></li>
+                                    <li><a href='blog-details.html'>Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href='contact.html'>Contact</a></li>
+                        </ul>
+                    </nav>
+                    <div class="header-right">
+                        <a class='default-btn d-none d-md-block' href='contact.html'>Start Free Trial</a>
+                        <button class="sidebar-trigger open">
+                            <svg class="trigger-opener" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="12.7px" viewBox="0 0 16 12.7" style="enable-background:new 0 0 16 12.7;" xml:space="preserve">
+                                <g>
+                                    <rect x="5" y="8.4" width="11" height="2" />
+                                    <rect x="0" y="2.4" width="16" height="2" />
+                                </g>
+                            </svg>
+                        </button>
+                        <div class="mobile-menu-icon">
+                            <div class="burger-menu">
+                                <div class="line-menu line-half first-line"></div>
+                                <div class="line-menu"></div>
+                                <div class="line-menu line-half last-line"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--/.main-header-->
+
+    <div id="sidebar-area" class="sidebar-area">
+        <button class="sidebar-trigger close">
+            <svg class="sidebar-close" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="12.7px" viewBox="0 0 16 12.7" style="enable-background:new 0 0 16 12.7;" xml:space="preserve">
+                <g>
+                    <rect x="0" y="5.4" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -2.1569 7.5208)" width="16" height="2"></rect>
+                    <rect x="0" y="5.4" transform="matrix(0.7071 0.7071 -0.7071 0.7071 6.8431 -3.7929)" width="16" height="2"></rect>
+                </g>
+            </svg>
+        </button>
+        <div class="sidebar-content">
+            <div class="site-logo">
+                <a href='index.html'><img src="<?= base_url('assets/img/logo-dark.png')?>" alt="logo"></a>
+            </div>
+            <p>9621 Agnes Crossing, Lake Suzanneview, New Mexico Island 84604-9295.</p>
+            <ul class="sidebar-info">
+                <li><span>Call:</span> 5267-214-392</li>
+                <li><span>Email:</span> <a href="https://te.dynamiclayers.net/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="f3ba9d959cddbf969297959f9c84b39e929a9fdd909c9e">[email&#160;protected]</a></li>
+            </ul>
+            <ul class="sidebar-social">
+                <li><a href="#"><i class="lab la-facebook-f"></i></a></li>
+                <li><a href="#"><i class="lab la-twitter"></i></a></li>
+                <li><a href="#"><i class="lab la-youtube"></i></a></li>
+                <li><a href="#"><i class="lab la-behance"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <!--/.sidebar-area-->
+
+    <!--/.sidebar-overlay-->
+    <div id="sidebar-overlay"></div>
